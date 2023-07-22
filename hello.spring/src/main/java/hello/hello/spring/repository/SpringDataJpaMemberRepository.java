d9fafa5b92acc4580d62c5a43248e18e23efaaa2
catspie92@gmail.com <catspie92@gmail.com>
2023-07-22 15:30:16 +0900

    "스프링 jpa 설명 주석"

--- hello.spring/src/main/java/hello/hello/spring/repository/SpringDataJpaMemberRepository.java
+++ hello.spring/src/main/java/hello/hello/spring/repository/SpringDataJpaMemberRepository.java
@@ -10,6 +10,8 @@ import java.util.Optional;
  */
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
+    //JPQL select m from Member m where m.name = ?
     @Override
     Optional<Member> findByName(String name);
+
 }
